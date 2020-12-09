b1950d8f03a42ec5200d07cb28776695dbef19d6
aahoogendoorn <aahoogendoorn@gmail.com>
2020-12-09 11:14:30 +0100

    test(Added test): Added test on MovieRepo.ts

---
+++ test/domain/MovieRepo.test.ts
@@ -0,0 +1,10 @@
+import { MovieRepo } from '../../src/domain/MovieRepo';
+import { OmdbGateway } from '../../src/data/OmdbGateway';
+
+describe('MovieRepo', () => {
+
+  test('new works', () => {
+    const repo = new MovieRepo();
+    expect(repo.movies).toBeInstanceOf(OmdbGateway);
+  });
+});

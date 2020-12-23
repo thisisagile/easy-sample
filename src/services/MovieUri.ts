f72f8a2deaa64f6fea0e1007c81ad167b6e7bea2
aahoogendoorn <aahoogendoorn@gmail.com>
2020-12-23 09:48:13 +0100

    fix(Added host to MovieUri.ts): Added localhost for now in MovieUri.ts, will replace by external variable later.

--- src/services/MovieUri.ts
+++ src/services/MovieUri.ts
@@ -2,7 +2,9 @@ import { Uri, uri } from '@thisisagile/easy';
 
 export class MovieUri extends Uri {
 
+  readonly host = uri.host("http://localhost:9001");
   readonly resource = uri.segment('movie');
+
   static readonly movies = uri.segment("movies");
 
   static readonly Movies = new MovieUri([MovieUri.movies]);

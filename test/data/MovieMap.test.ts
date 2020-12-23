2616e7f587d315f23e41596cdc385277dd78afc4
aahoogendoorn <aahoogendoorn@gmail.com>
2020-12-23 16:28:05 +0100

    fix(Added tests): Added tests to Movie.test.ts and MovieMap.test.ts

--- test/data/MovieMap.test.ts
+++ test/data/MovieMap.test.ts
@@ -1,26 +1,12 @@
 import { MovieMap } from '../../src/data/MovieMap';
+import { movie, omdb } from '../ref/data';
 
 describe('MovieMap', () => {
-
-  const data =  {
-    Title: "Monty Python's Life of Brian",
-    Year: "1979",
-    imdbID: "tt0079470",
-    Type: "movie",
-    Poster: "https://m.media-amazon.com/images/M/MV5BMzAwNjU1OTktYjY3Mi00NDY5LWFlZWUtZjhjNGE0OTkwZDkwXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-  };
-
-  const movie =  {
-    id: "tt0079470",
-    title: "Monty Python's Life of Brian",
-    year: "1979",
-    poster: "https://m.media-amazon.com/images/M/MV5BMzAwNjU1OTktYjY3Mi00NDY5LWFlZWUtZjhjNGE0OTkwZDkwXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-  };
 
   const map = new MovieMap();
 
   test('It works', () => {
-    const brian = map.in(data);
+    const brian = map.in(omdb);
     expect(brian).toMatchObject(movie);
   });
 });
--- test/domain/Movie.test.ts
+++ test/domain/Movie.test.ts
@@ -1,19 +1,20 @@
-import movies from "../ref/movies.json";
 import { Movie } from '../../src/domain/Movie';
 import "@thisisagile/easy-test";
+import movies from "../ref/movies.json";
+import omdb from "../ref/omdb.json";
 
 describe('Movie', () => {
 
   test('Movie maps correctly', () => {
-    const m = new Movie(movies[0]);
-    expect(m.id).toBe(movies[0].imdbID);
-    expect(m.title).toBe(movies[0].Title);
-    expect(m.year).toBe(movies[0].Year);
-    expect(m.poster).toBe(movies[0].Poster);
+    const m = new Movie(omdb[0]);
+    expect(m.id).toBe(omdb[0].imdbID);
+    expect(m.title).toBe(omdb[0].Title);
+    expect(m.year).toBe(omdb[0].Year);
+    expect(m.poster).toBe(omdb[0].Poster);
   });
 
   test("Movie is valid", () => {
-    const m = new Movie(movies[0]);
+    const m = new Movie(omdb[0]);
     expect(m).toBeValid();
   });
 
@@ -21,4 +22,10 @@ describe('Movie', () => {
     const m = new Movie({ Title: "Man on the Moon" });
     expect(m).not.toBeValid();
   });
+
+  test("Movie from data is correct", () => {
+    const m = new Movie(omdb[0]);
+    expect(m.id).toBe("tt0079470");
+    expect(m.toJSON()).toEqual(movies[0]);
+  })
 });

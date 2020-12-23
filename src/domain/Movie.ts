282c079ec8a469e90c9e5bea05a198b5d48c5901
aahoogendoorn <aahoogendoorn@gmail.com>
2020-12-23 09:47:12 +0100

    fix(Added update() to Movie): Added update method (though we cannot call the external service, as an example.

--- src/domain/Movie.ts
+++ src/domain/Movie.ts
@@ -1,8 +1,10 @@
-import { Entity, Id, required } from '@thisisagile/easy';
+import { Entity, Id, Json, required } from '@thisisagile/easy';
 
 export class Movie extends Entity {
   @required() readonly id: Id = this.state.imdbID;
   @required() readonly title: string = this.state.Title;
   @required() readonly year: number = this.state.Year;
   readonly poster: string = this.state.Poster;
+
+  update = (add?: Json): Movie => new Movie(this.toJSON(add));
 }

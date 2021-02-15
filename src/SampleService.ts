d582f63bd92b8c5c40f0300e9a9fa2c503822ee6
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-15 09:28:03 +0100

    fix(sampleservice): updated start method.

--- src/SampleService.ts
+++ src/SampleService.ts
@@ -1,8 +1,13 @@
-import { error, Handler, notFound, Service } from '@thisisagile/easy';
+import { error, Handler, HealthResource, notFound, Service } from '@thisisagile/easy';
 
 export class SampleService extends Service {
 
   static readonly Movie = new SampleService('movie');
 
   post = (): Handler[] => [notFound, error];
+
+  start(): void {
+    this.with(HealthResource);
+    super.start();
+  };
 }

a1cc1b95a65ed0f0db49c10b30684f3c84fd044b
aahoogendoorn <aahoogendoorn@gmail.com>
2021-02-15 09:28:16 +0100

    fix(sampleservice): updated app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
 import { MoviesResource } from './services/MoviesResource';
 import { MovieResource } from './services/MovieResource';
-import { HealthResource } from '@thisisagile/easy';
 import { SampleService } from './SampleService';
 
 SampleService.Movie
   .with(MoviesResource, MovieResource)
-  .with(HealthResource)
   .atPort(9001)
   .start();
